dd34343b1b3b3ef62ecdb20164546a5536b81223
Patel Payal <pp0201733@gmail.com>
2026-03-30 09:12:19 +0530

    Assessment15
    Assessment15

--- ASP.NET/Program.cs
+++ ASP.NET/Program.cs
@@ -1,13 +1,73 @@
+using Microsoft.EntityFrameworkCore;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using OptimizedAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<EmailService>();
-builder.Services.AddSingleton<SmsService>();
 
-builder.Services.AddHostedService<BackgroundNotificationService>();
+// Database Connection (InMemory for testing)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("UserDB"));
+
+
+// Response Caching
+builder.Services.AddResponseCaching();
+
+
+// CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+// Rate Limiting (5 requests per minute per IP)
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5;
+        limiterOptions.Window = TimeSpan.FromMinutes(1);
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = 2;
+    });
+});
+
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+// Global Error Handling
+app.UseExceptionHandler("/error");
+
+
+// Middleware Pipeline
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
+app.UseResponseCaching();
+
+app.UseRateLimiter();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
